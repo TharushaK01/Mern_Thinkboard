ced87566a5dc50f1f33877c68c0f61bf5e52aba7
TharushaK01 <tharushakavinda01@gmail.com>
2025-08-20 15:47:11 +0530

    Home page finished

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,7 @@ import RateLimitedUI from '../components/RateLimitedUI';
 import { useEffect } from "react";
 import axios from "axios";
 import { toast } from 'react-hot-toast';
+import NoteCard from '../components/NoteCard';
 
 
 const HomePage = () => {
@@ -34,13 +35,26 @@ const HomePage = () => {
     fetchNotes();
   }, []);
 
-  return (
-    <div className='min-h-screen'>
+ return (
+    <div className="min-h-screen">
       <Navbar />
 
-      {isRateLimited && <RateLimitedUI/>}
-    </div>
-  )
-}
+      {isRateLimited && <RateLimitedUI />}
 
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+
+        {/* {notes.length === 0 && !isRateLimited && <NotesNotFound />} */}
+
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
+            ))}
+          </div> 
+        )}
+      </div>
+    </div>
+  );
+};
 export default HomePage;
